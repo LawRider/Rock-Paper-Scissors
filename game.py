05c857cc7bce1d8e4012b59b29435e7b09f62ca5
Oleksandr <9040602+LawRider@users.noreply.github.com>
2020-11-16 03:03:49 +0200

    Update game.py

--- game.py
+++ game.py
@@ -1,27 +1,26 @@
 from random import choice
 
 
-class TicTacToe:
+class RockPaperScissors:
     def __init__(self):
-        self.game_list = ['paper', 'scissors', 'rock']
-        self.win = {"rock": "scissors", "scissors": "paper", "paper": "rock"}
+        self.default_options = ['paper', 'scissors', 'rock']
         self.score = 0
         self.user_name = ''
 
     def open_file(self, filename):
         with open(filename, 'r') as file:
             for line in file:
-                data = line.split()
-                if data[0] == self.user_name:
-                    self.score = int(data[1])
+                name, score = line.split()
+                if name == self.user_name:
+                    self.score = int(score)
 
     def start_game(self):
         self.user_name = input("Enter your name: ")
-        print(f"Hello, {self.user_name}")
+        print(f"Hello, {self.user_name.title()}")
         self.open_file('rating.txt')
-        options = input()
-        if options:
-            self.game_list = options.split(',')
+        custom_options = input()
+        if custom_options:
+            self.default_options = custom_options.split(',')
         print("Okay, let's start")
         while True:
             user_input = input()
@@ -31,24 +30,24 @@ class TicTacToe:
             elif user_input == '!rating':
                 print(f"Your rating: {self.score}")
                 continue
-            elif user_input not in self.game_list:
+            elif user_input not in self.default_options:
                 print("Invalid input")
                 continue
             else:
-                user_index = self.game_list.index(user_input)
-                new_list = self.game_list[user_index + 1:] + self.game_list[:user_index]
-                half = len(new_list) // 2
-                pc_input = choice(self.game_list)
+                user_index = self.default_options.index(user_input)
+                modified_list = self.default_options[user_index + 1:] + self.default_options[:user_index]
+                half_list = len(modified_list) // 2
+                pc_input = choice(self.default_options)
                 if user_input == pc_input:
                     print(f"There is a draw ({pc_input})")
                     self.score += 50
-                elif pc_input in new_list[half:]:
+                elif pc_input in modified_list[half_list:]:
                     print(f"Well done. The computer chose {pc_input} and failed")
                     self.score += 100
-                elif pc_input in new_list[:half]:
+                else:
                     print(f"Sorry, but the computer chose {pc_input}")
 
 
 if __name__ == '__main__':
-    game = TicTacToe()
+    game = RockPaperScissors()
     game.start_game()
